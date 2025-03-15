2e97ecc677cee1461f0d03dada09d792fb2b6a63
Stevenberger1 <Stevenberger1807@gmail.com>
2025-03-15 15:39:13 +0200

    Reorganized project structure and added src module

---
+++ src/model.py
@@ -0,0 +1,10 @@
+
+
+
+def train_model(data):
+    """Trains a simple stock prediction model."""
+
+
+def predict(model, new_data):
+    """Uses the trained model to predict stock prices."""
+    return model.predict(new_data)
