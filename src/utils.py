1693029db5f8e3d649c1a16cdd612841984d5506
Stevenberger1 <Stevenberger1807@gmail.com>
2025-03-15 16:56:23 +0200

    write the updates here

---
+++ src/utils.py
@@ -0,0 +1,3 @@
+#This file contains small functions that are useful across multiple files.
+
+#Instead of writing logging functions inside every file, we define them once in utils.py and reuse them.
